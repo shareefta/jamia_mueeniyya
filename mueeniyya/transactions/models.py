4ae6578a5010d29027e83a776dbeee4fc7379e45
shareefta <shareefta@gmail.com>
2025-10-23 08:46:31 +0530

    Updated OB Model

--- mueeniyya/transactions/models.py
+++ mueeniyya/transactions/models.py
@@ -66,5 +66,5 @@ class OpeningBalance(models.Model):
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.campus} - {self.amount}"
+        return f"{self.cash_book} - {self.amount}"
 
