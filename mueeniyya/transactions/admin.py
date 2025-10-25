ea542180fa9624dcecfbd2b9596edf79846d6022
shareefta <shareefta@gmail.com>
2025-10-25 09:30:24 +0530

    Updated Admin.py - transactions app

--- mueeniyya/transactions/admin.py
+++ mueeniyya/transactions/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category, PaymentMode, Transaction, OpeningBalance, CashBook
+from .models import Category, PaymentMode, Transaction, OpeningBalance, CashBook, Party
 
 admin.site.register(Category)
 admin.site.register(PaymentMode)
 admin.site.register(Transaction)
 admin.site.register(OpeningBalance)
 admin.site.register(CashBook)
+admin.site.register(Party)
