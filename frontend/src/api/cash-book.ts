251b7823ed34edaa8bc7daf2728a3ae0a1b47eae
shareefta <shareefta@gmail.com>
2025-10-29 16:47:16 +0530

    updateded base APIs

--- frontend/src/api/cash-book.ts
+++ frontend/src/api/cash-book.ts
@@ -1,10 +1,6 @@
-import axios from "axios";
+import api from "src/utils/api";
 
-const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}transactions/cash_books/`;
-
-const getAuthHeaders = () => ({
-  Authorization: `Bearer ${localStorage.getItem("token")}`,
-});
+const BASE_PATH = "transactions/cash_books/";
 
 export interface CashBookProps {
   id?: number;
@@ -15,20 +11,20 @@ export interface CashBookProps {
 
 // Get all Cash Books
 export const getCashBooks = async () => {
-  const res = await axios.get(BASE_URL, { headers: getAuthHeaders() });
+  const res = await api.get(BASE_PATH);
   return res.data;
 };
 
 // Create a new Cash Book
 export const createCashBook = async (data: CashBookProps) => {
-  const res = await axios.post(BASE_URL, data, { headers: getAuthHeaders() });
+  const res = await api.post(BASE_PATH);
   window.dispatchEvent(new Event("cashbook-update"));
   return res.data;
 };
 
 // Update a Cash Book
 export const updateCashBook = async (id: number, data: CashBookProps) => {
-  const res = await axios.put(`${BASE_URL}${id}/`, data, { headers: getAuthHeaders() });
+  const res = await api.put(`${BASE_PATH}${id}/`, data);
   window.dispatchEvent(new Event("cashbook-update"));
   return res.data;
 };
@@ -36,7 +32,7 @@ export const updateCashBook = async (id: number, data: CashBookProps) => {
 // Delete a Cash Book
 export const deleteCashBook = async (id: number) => {
   try {
-    const res = await axios.delete(`${BASE_URL}${id}/`, { headers: getAuthHeaders() });
+    const res = await api.delete(`${BASE_PATH}${id}/`);
     window.dispatchEvent(new Event("cashbook-update"));
     return res.data;
   } catch (error: any) {
